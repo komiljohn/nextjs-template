97fbf6daf1fa4a6c1dd4e8398a968f0bb4a6ad9f
komiljon_yormurodov <komiljonyormurodov@gmail.com>
2025-01-13 18:36:20 +0500

    configure color variables on global.css, add button component

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,8 +2,19 @@
 
 import { useTranslations } from "next-intl";
 
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   const t = useTranslations("Home");
 
-  return <div>{t("title")}</div>;
+  return (
+    <div>
+      {t("title")}
+      <div className="p-[400px] space-x-2">
+        <Button variant="secondary">Cancel</Button>
+        <Button variant="outline">Outline</Button>
+        <Button>Submit</Button>
+      </div>
+    </div>
+  );
 }
